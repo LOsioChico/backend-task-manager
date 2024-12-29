2403a524dd3a0ab0e7e421cb5690e88765f27685
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-12-29 18:35:58 -0500

    feat: Add Swagger API documentation configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,3 +11,20 @@ export const config = {
     max: 100, // limit each IP to 100 requests per windowMs
   },
 };
+
+export const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Task Manager API",
+      version: "1.0.0",
+      description: "A simple Task Manager API",
+    },
+    servers: [
+      {
+        url: `http://localhost:${config.port}`,
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { type Request, type Response } from "express";
 import { rateLimit } from "express-rate-limit";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 
-import { config } from "./config";
+import { config, swaggerOptions } from "./config";
 import { httpLogger, logger } from "./utils/logger";
 
 const app = express()
@@ -10,6 +12,9 @@ const app = express()
   .use(rateLimit(config.rateLimit))
   .use(cors({ origin: config.corsOrigins }))
   .use(httpLogger);
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
