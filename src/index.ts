e69cd72cfc93d5d4f29664ed7e85f054b80ec234
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-12-29 18:32:32 -0500

    feat: Add logging with Winston and Morgan

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,19 @@ import { rateLimit } from "express-rate-limit";
 import cors from "cors";
 
 import { config } from "./config";
+import { httpLogger, logger } from "./utils/logger";
 
 const app = express();
 
 app.use(express.json());
 app.use(rateLimit(config.rateLimit));
 app.use(cors({ origin: config.corsOrigins }));
+app.use(httpLogger);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
 app.listen(config.port, () => {
-  console.log(`Server is running on port ${config.port}`);
+  logger.info(`Server is running on port ${config.port}`);
 });
---
+++ src/utils/logger.ts
@@ -0,0 +1,29 @@
+import winston from "winston";
+import morgan from "morgan";
+
+import { config } from "../config";
+
+const { combine, timestamp, printf, colorize } = winston.format;
+
+const readableFormat = printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level}: ${message}`;
+});
+
+export const logger = winston.createLogger({
+  level: config.environment === "development" ? "debug" : "info",
+  format: combine(
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    config.environment === "development" ? combine(colorize(), readableFormat) : winston.format.json(),
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+const morganFormat = config.environment === "development" ? ":method :url :status :response-time ms" : "combined";
+
+export const httpLogger = morgan(morganFormat, {
+  stream: {
+    write: (message) => {
+      logger.info(message.trim());
+    },
+  },
+});
